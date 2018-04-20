ac5c07d7b9515e9874b1318165f6e8a60d2e2b50
difr <dialfr@gmail.com>
2018-04-21 02:53:58 +0300

    land: thermal: Revision [1/2]

--- device.mk
+++ device.mk
@@ -385,10 +385,8 @@ PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
 # Thermal
-PRODUCT_PACKAGES += \
-    thermal.msm8937 \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # USB HAL
 PRODUCT_PACKAGES += \
