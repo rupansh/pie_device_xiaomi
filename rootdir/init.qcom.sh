b9de1d42a00539970ae3cf9accd4b79a0a3c2d3f
Ritesh Saxena <riteshsax007@gmail.com>
2018-08-12 12:43:13 +0000

    land: Add ims hex
    Change-Id: I8c0d9e32b5dd5efc99593e9633b9c3a1e90ea02c

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -81,6 +81,12 @@ case "$buildvariant" in
         ;;
 esac
 
+#IMS HEX
+if [ -f /data/system/users/0/settings_global.xml ]; then
+    sed -i 's/"multi_sim_data_call" value="1"/"multi_sim_data_call" value="-1"/g' /data/system/users/0/settings_global.xml
+    restorecon /data/system/users/0/settings_global.xml
+fi
+
 # Create /persist/alarm if necessary
 if [ ! -d /persist/alarm ]; then
     mkdir /persist/alarm
