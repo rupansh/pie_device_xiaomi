2c1d63daa760bd8946b65afb3caedd7db66d39fa
argraur <argraur@gmail.com>
2018-08-15 18:20:13 +0000

    audio-hal: fallback to 2.0
    ref: https://github.com/NexusPieX/device_lge_bullhead/commit/e93b25794bb002975dd82867a264478334c8fcf4
    Signed-off-by: rupansh <rupanshsekar@hotmail.com>

--- device.mk
+++ device.mk
@@ -99,10 +99,10 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl:32 \
-    android.hardware.audio@4.0-service \
-    android.hardware.audio.effect@4.0-impl:32 \
-    android.hardware.soundtrigger@2.1-impl:32
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 # Audio mixer
 PRODUCT_COPY_FILES += \
