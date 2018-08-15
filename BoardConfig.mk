cd331ad53be97cb00ad45d38a04b19eee6f11f36
rupansh <rupanshsekar@hotmail.com>
2018-08-15 19:52:05 +0000

    Nuke qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -217,7 +217,6 @@ TARGET_RIL_VARIANT := caf
 TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Wi-Fi
