eac96e23c688056be65f68ea1218cb99db80c63a
argraur <argraur@gmail.com>
2018-08-15 17:20:50 +0000

    device: build libbt-vendor from source
    ref: https://github.com/NexusPieX/device_lge_bullhead/commit/97998e0096038ca27eeca23ec448ebae1209be72

--- device.mk
+++ device.mk
@@ -130,7 +130,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl-qti:64 \
     android.hardware.bluetooth@1.0-service-qti \
-    android.hardware.bluetooth@1.0-service-qti.rc
+    android.hardware.bluetooth@1.0-service-qti.rc \
+    libbt-vendor
 
 # Bluetooth SoC
 PRODUCT_PROPERTY_OVERRIDES += \
