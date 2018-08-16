9c6039140f1223d2eaa8bca309cdcd616a079b6a
RiteshSaxena <riteshsax007@gmail.com>
2018-08-16 18:24:53 +0000

    init: Modify for P
    tenks ritesh sur
    
    Signed-off-by: rupansh <rupanshsekar@hotmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -19,16 +19,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
-    system/core/base/include \
-    system/core/init \
-    external/selinux/libselinux/include
-
-LOCAL_CFLAGS := -Wall
-
+        system/core/base/include \
+        system/core/init \
+        external/selinux/libselinux/include
 LOCAL_MODULE := libinit_land
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_land.cpp
-
-LOCAL_STATIC_LIBRARIES := libbase
+LOCAL_STATIC_LIBRARIES := libbase libselinux
 
 include $(BUILD_STATIC_LIBRARY)
--- init/init_land.cpp
+++ init/init_land.cpp
@@ -1,6 +1,5 @@
 /*
-   Copyright (c) 2016, The CyanogenMod Project
-   Copyright (c) 2017, The XPerience Project
+   Copyright (c) 2017, The LineageOS Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -28,86 +27,31 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <android-base/properties.h>
-#include <android-base/logging.h>
-#include <fcntl.h>
+#include <fstream>
+#include <stdio.h>
 #include <stdlib.h>
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
-#include <iostream>
-#include <string>
-#include <sstream>
+#include <string.h>
 #include <sys/sysinfo.h>
 
-//for cmdline parsing
 #include <android-base/file.h>
+#include <android-base/properties.h>
 #include <android-base/strings.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
-
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
+#include "vendor_init.h"
 
 static std::string board_id;
+
 using android::base::GetProperty;
 using android::base::Trim;
 using android::init::property_set;
-using android::init::import_kernel_cmdline;
 
-//Take care about 3gb ram
-int is3GB()
-{
-    struct sysinfo sys;
-    sysinfo(&sys);
-    return sys.totalram > 2048ull * 1024 * 1024;
-}
-
-//cmdline parser
-void import_kernel_cmdline1(bool in_qemu,
-                           const std::function<void(const std::string&, const std::string&, bool)>& fn) {
-    std::string cmdline;
-    android::base::ReadFileToString("/proc/cmdline", &cmdline);
-
-    for (const auto& entry : android::base::Split(android::base::Trim(cmdline), " ")) {
-        std::vector<std::string> pieces = android::base::Split(entry, "=");
-        if (pieces.size() >= 2) { // lineage's : == 2
-            fn(pieces[0], pieces[1], in_qemu);
-        }
-    }
-}
-
-
-static void import_cmdline(const std::string& key,
-        const std::string& value, bool for_emulator __attribute__((unused)))
-{
-    if (key.empty()) return;
-
-    if (key == "board_id") {
-        std::istringstream iss(value);
-        std::string token;
-        std::getline(iss, token, ':');
-        board_id = token;
-    }
-}
-
-static void init_alarm_boot_properties()
-{
+static void init_alarm_boot_properties(){
     char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
     char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
     std::string boot_reason;
     std::string power_off_alarm;
-    std::string reboot_reason = GetProperty("ro.boot.alarmboot","");
+    std::string tmp = GetProperty("ro.boot.alarmboot", "");
 
     if (android::base::ReadFileToString(boot_reason_file, &boot_reason)
             && android::base::ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
@@ -121,105 +65,43 @@ static void init_alarm_boot_properties()
          * 2 -> sudden momentary power loss (SMPL)
          * 3 -> real time clock (RTC)
          * 4 -> DC charger inserted
-         * 5 -> USB charger inserted
+         * 5 -> USB charger insertd
          * 6 -> PON1 pin toggled (for secondary PMICs)
          * 7 -> CBLPWR_N pin toggled (for external power supply)
          * 8 -> KPDPWR_N pin toggled (power key pressed)
          */
-         if ((Trim(boot_reason) == "3" || reboot_reason == "true")
-                 && Trim(power_off_alarm) == "1") {
-             property_set("ro.alarm_boot", "true");
-         } else {
-             property_set("ro.alarm_boot", "false");
-         }
+        if ((Trim(boot_reason) == "3" || tmp == "true")
+                && Trim(power_off_alarm) == "1")
+            property_set("ro.alarm_boot", "true");
+        else
+            property_set("ro.alarm_boot", "false");
     }
 }
 
-void read_ramconfig()
-{
-    if (is3GB()) {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "288m");
-        property_set("dalvik.vm.heapsize", "768m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "512k");
-        property_set("dalvik.vm.heapmaxfree", "8m");
-        //HWUI
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "1024");
-    } else {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "192m");
-        property_set("dalvik.vm.heapsize", "512m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "2m");
-        property_set("dalvik.vm.heapmaxfree", "8m");
-        //HWUI
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "2048");
-    }
-}
+void init_variant_properties(){
+    std::ifstream fin;
+    std::string buf;
 
-void variant_properties()
-{
     std::string product = GetProperty("ro.product.name", "");
     if (product.find("land") == std::string::npos)
         return;
 
-    import_kernel_cmdline1(0, import_cmdline);
+    fin.open("/proc/cmdline");
+    while (std::getline(fin, buf, ' '))
+        if (buf.find("board_id") != std::string::npos)
+            break;
+    fin.close();
 
-    //set board
-    property_set("ro.product.wt.boardid", board_id.c_str());
-
-
-    //Variants
-    if (board_id == "S88537AA1") {
-        property_set("ro.build.display.wtid", "SW_S88537AA1_V090_M20_MP_XM");
-    } else if (board_id == "S88537AB1") {
-        property_set("ro.build.display.wtid", "SW_S88537AB1_V090_M20_MP_XM");
-    } else if (board_id == "S88537AC1") {
-        property_set("ro.build.display.wtid", "SW_S88537AC1_V090_M20_MP_XM");
-    } else if (board_id == "S88537BA1") {
-        property_set("ro.build.display.wtid", "SW_S88537BA1_V090_M20_MP_XM");
-    } else if (board_id == "S88537CA1") {
-        property_set("ro.build.display.wtid", "SW_S88537CA1_V090_M20_MP_XM");
-    } else if (board_id == "S88537EC1") {
-        property_set("ro.build.display.wtid", "SW_S88537EC1_V090_M20_MP_XM");
-    }
-
-    if (board_id == "S88537AB1") {
-        property_override("ro.product.model", "Redmi 3X");
-        property_override("ro.vendor.product.device", "Redmi 3X");
+    if (buf.find("S88537AB1") != std::string::npos) {
+        property_set("ro.product.model", "Redmi 3X");
+        property_set("ro.vendor.product.model", "Redmi 3X");
     } else {
-        property_override("ro.product.model", "Redmi 3S");
-        property_override("ro.vendor.product.device", "Redmi 3S");
+        property_set("ro.product.model", "Redmi 3S");
+        property_set("ro.vendor.product.model", "Redmi 3X");
     }
-
 }
 
-void vendor_load_properties()
-{
-
-    // init
-    read_ramconfig();
+void vendor_load_properties(){
     init_alarm_boot_properties();
-    variant_properties();
+    init_variant_properties();
 }
